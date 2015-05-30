4533cfec1ef57c1301388f477f1f907ae60672e1
Andrew Williams <andrew@manor.org>
2015-05-30 13:19:13 -0400

    Move the coverage directory to top level out of test
    fixes #13

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
             coverage: {
                 src: ['test/**/*.test.js'],
                 options: {
-                    coverageFolder: 'test/coverage'
+                    coverageFolder: 'coverage'
                 }
             }
         },
