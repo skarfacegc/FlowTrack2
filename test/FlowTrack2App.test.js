a3f39356d388a88c94fb937ff79f527ffbeadaad
Andrew Williams <andrew@manor.org>
2015-06-06 09:38:15 -0400

    Add route test for /bower
    Tagging issue #3

--- test/FlowTrack2App.test.js
+++ test/FlowTrack2App.test.js
@@ -5,15 +5,29 @@ var GetLogger = require('../lib/GetLogger');
 var request = require('supertest');
 
 describe('FlowTrack2App', function() {
-    it('/ route should return html', function(done) {
+    describe('Routes', function() {
+        it('/ route should return html', function(done) {
 
-        var app = new FlowTrack2App(new GetLogger('test', 'TestLogger'));
+            var app = new FlowTrack2App(new GetLogger('test', 'TestLogger'));
 
-        request(app)
-            .get('/')
-            .expect('Content-type', /text\/html.*/)
-            .expect(200, done);
+            request(app)
+                .get('/')
+                .expect('Content-type', /text\/html.*/)
+                .expect(200, done);
 
+        });
+
+        // Just use angular as a sample, not actually testing angular, just making sure
+        // the /bower route works
+        it('/bower/angular/angular.js should return javascript', function(done) {
+
+            var app = new FlowTrack2App(new GetLogger('test', 'TestLogger'));
+
+            request(app)
+                .get('/bower/angular/angular.js')
+                .expect('Content-type', /.*javascript.*/)
+                .expect(200, done);
+        });
     });
 
 });
