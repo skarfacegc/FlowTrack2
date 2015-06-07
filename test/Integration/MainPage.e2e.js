c13ebd4dde90cd2877b61ddf81295624d8472af4
Andrew Williams <andrew@manor.org>
2015-06-06 22:30:36 -0400

    [checkpoint] Basic protractor test working.
    Still a bunch to do, but I have a basic test running.  Next step is to
    get it hooked up in grunt and add coverage.

---
+++ test/Integration/MainPage.e2e.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+var expect = chai.expect;
+
+describe('Main Page', function() {
+    it('should have a title', function(){
+        browser.get('http://localhost:3000');
+
+        expect(browser.getTitle()).to.eventually.equal('FlowTrack');
+    });
+});
---
+++ test/Integration/e2e.conf.js
@@ -0,0 +1,6 @@
+// conf.js
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  framework: 'mocha',
+  specs: ['*.e2e.js']
+}
