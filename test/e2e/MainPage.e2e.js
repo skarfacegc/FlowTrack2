8384d28db34deb6afa2616393c503b78b123b5bd
Andrew Williams <andrew@manor.org>
2015-06-20 14:09:36 -0400

    e2e is working
    Starting the e2e library, will likely split it into a separate package
    at some point, leaving it here for now.

--- test/e2e/MainPage.e2e.js
+++ test/e2e/MainPage.e2e.js
@@ -3,14 +3,22 @@
 
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
 
+var UiGridTest = require('./UiGridTest');
+
+chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 describe('Main Page', function() {
-    it('should have a title', function(){
+    it('should have a title', function() {
         browser.get('http://localhost:3000');
 
         expect(browser.getTitle()).to.eventually.equal('FlowTrack');
+    });
+
+    it('should have 5 header columns', function() {
+        var grid = new UiGridTest('mainGrid');
+        var headers = grid.getColumnHeaders('mainGrid');
+        expect(headers.count()).to.eventually.equal(5);
     });
 });
---
+++ test/e2e/UiGridTest.js
@@ -0,0 +1,31 @@
+/* globals element,by*/
+'use strict';
+
+
+// constructor
+// 
+// Set the grid element to work on
+function UiGridTest(gridId) {
+    var gridElement;
+
+    // Assume string is an ID
+    if (typeof(gridId) === 'string') {
+        gridElement = element(by.id(gridId));
+    } else {
+        gridElement = element(gridId);
+    }
+
+    this.grid = gridElement;
+}
+
+//
+// return the header object
+UiGridTest.prototype.getColumnHeaders = function() {
+    var headerCols = this.grid
+        .element(by.css('.ui-grid-header-cell-row'))
+        .all(by.repeater('col in colContainer.renderedColumns track by col.colDef.name'));
+    return headerCols;
+};
+
+
+module.exports = UiGridTest;
