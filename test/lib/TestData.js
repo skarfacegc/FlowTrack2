e4c69379d6c641cd7a39c41450743635e7eddd12
Andrew Williams <andrew@manor.org>
2016-04-16 22:48:14 -0400

    Add TestData library
    related to issue #73

---
+++ test/lib/TestData.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var EsConnect = require('../../lib/model/EsConnect');
+var NetFlowStorage = require('../../lib/model/NetFlowStorage');
+var IndexTrack = require('../../lib/model/IndexTracking');
+
+function TestData(es, logger, config) {
+    var esConn = new EsConnect(es, logger, config);
+    this.client = esConn.connect();
+
+    this.nfStore = new NetFlowStorage(es, logger, config);
+    this.indexTrack = new IndexTrack(es, logger, config);
+    this.logger = logger;
+    this.config = config;
+
+    this.defaultCount = 100;
+    this.defaultStep = 1000;
+}
+
+
+
+
+// load the test data into the database
+// will load count records in the database decreasing time by step ms
+// if flowRecord isn't defined we'll load data from this.getFlowRecord
+// if flowRecord is a function we'll load data from that
+// if flowRecord is an object we'll use that
+TestData.prototype.simpleLoadData = function (count, step, flowRecord) {
+    var sampleFlow = {};
+
+    // if flowRecord isn't defined load from the default generator
+    // if flowRecord is a function invoke the function and use that
+    // if flowRecord is an object use that directly
+    // otherwise error!
+    if (flowRecord === null || flowRecord === undefined) {
+        sampleFlow = this.getFlowRecord();
+    } else if (typeof (flowRecord) === 'function') {
+        sampleFlow = flowRecord();
+    } else if (typeof (flowRecord) === 'object') {
+        sampleFlow = flowRecord;
+    } else {
+        this.logger.error('Invalid flow record: ' + flowRecord);
+        throw 'Invalid flow record';
+    }
+
+    for (var i = 0; i <= count; i++) {
+        sampleFlow.timestamp = Date.now() - (i * step);
+        this.nfStore.storeFlow(sampleFlow);
+    }
+};
+
+// Sample flow
+TestData.prototype.getFlowRecord = function () {
+    var simpleFlow = {
+        "ipv4_src_addr": '192.168.1.1',
+        "ipv4_dst_addr": '192.168.1.2',
+        "ipv4_next_hop": 0,
+        "input_snmp": 1,
+        "output_snmp": 0,
+        "in_pkts": 2,
+        "in_bytes": 402,
+        "first_switched": 4294967295,
+        "last_switched": 4294967295,
+        "ipv4_src_port": 60521,
+        "ipv4_dst_port": 1900,
+        "tcp_flags": 0,
+        "protocol": 17,
+        "src_tos": 0,
+        "in_as": 0,
+        "out_as": 0,
+        "src_mask": 0,
+        "dst_mask": 0
+    };
+
+    return simpleFlow;
+};
+
+// Delete all indices
+TestData.prototype.deleteTestData = function () {
+    var indexTrack = this.indexTrack;
+    indexTrack.getIndexList(function (indexList) {
+        indexTrack.deleteIndices(indexList, function () {
+            return;
+        });
+    });
+};
+
+module.exports = TestData;
